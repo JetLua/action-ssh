8f92e6000fdbf2756b1d89202c445884b25233a7
无忘 <wuwang@yige-group.com>
2022-08-04 16:00:26 +0800

    update

--- index.mjs
+++ index.mjs
@@ -1,19 +1,45 @@
+import {env} from 'node:process'
+import {relative} from 'node:path'
+import {stat} from 'node:fs/promises'
+import {createReadStream} from 'node:fs'
+
+import 'dotenv/config'
+import qiniu from 'qiniu'
 import glob from '@actions/glob'
 import core from '@actions/core'
 
 
-const accessKey = core.getInput('access_key')
-const secretKey = core.getInput('secret_key')
-const bucket = core.getInput('bucket')
-const srcDir = core.getInput('source_dir')
-const dest_dir = core.getInput('dest_dir')
+const accessKey = core.getInput('access_key') ?? env.ACCESS_KEY
+const secretKey = core.getInput('secret_key') ?? env.SECRET_KEY
+const bucket = core.getInput('bucket') ?? env.BUCKET
+const sourceDir = core.getInput('source_dir') ?? env.SOURCE_DIR
+const destDir = core.getInput('dest_dir') ?? env.DEST_DIR
 
 const globber = await glob.create([
-  `${srcDir}/**/*`,
+  `${srcDir}/*`
 ].join('\n'))
 
+const mac = new qiniu.auth.digest.Mac(accessKey, secretKey)
+const uploader = new qiniu.form_up.FormUploader()
 
 
 for await (const file of globber.globGenerator()) {
-  console.log(file)
+  const stats = await stat(file)
+  if (stats.isDirectory()) continue
+  upload(file)
+}
+
+function upload(file) {
+  const key = `${destDir}/${relative(sourceDir, file)}`
+
+  const policy = new qiniu.rs.PutPolicy({
+    scope: `${bucket}:${key}`,
+  })
+
+  const token = policy.uploadToken(mac)
+
+  uploader.putStream(token, key, createReadStream(file), null, (err) => {
+    if (err) console.log(err.message)
+    console.log(`ok: ${key}`)
+  })
 }
---
+++ ssh.mjs
@@ -0,0 +1,4 @@
+import {env} from 'node:process'
+import 'dotenv/config'
+
+console.log(env.SSH_KEY)
