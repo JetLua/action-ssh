1b19d4669977fe10fcc64b17cb37c502004ab06e
JetLu <i@lufei.im>
2022-08-08 02:54:15 +0800

    update

--- index.ts
+++ index.ts
@@ -1,36 +1,17 @@
 import {relative} from 'node:path'
-import {stat} from 'node:fs/promises'
+import {readFile, stat, writeFile} from 'node:fs/promises'
 
-import {Client} from 'ssh2'
+import axios from 'axios'
+import * as FormData from 'form-data'
+import * as JSZip from 'jszip'
 import * as core from '@actions/core'
 import * as glob from '@actions/glob'
 
-import type { SFTPWrapper } from 'ssh2'
+const URL = core.getInput('URL')
+const DIR = core.getInput('DIR')
 
-const SSH_KEY = core.getInput('SSH_KEY')
-const SSH_HOST = core.getInput('SSH_HOST')
-const SSH_PORT = +core.getInput('SSH_PORT')
-const SSH_USER = core.getInput('SSH_USER')
-const SSH_DIR = core.getInput('SSH_DIR')
-
-const client = new Client()
 
 !async function() {
-  const sftp = await new Promise<SFTPWrapper>((resolve, reject) => {
-    client.connect({
-      privateKey: SSH_KEY,
-      port: SSH_PORT,
-      host: SSH_HOST,
-      username: SSH_USER
-    }).on('ready', () => {
-      client.sftp((err, sftp) => {
-        if (err) reject(err)
-        resolve(sftp)
-      })
-    })
-  })
-
-
   const globber = await glob.create([
     '.next/**/*',
     'public/**/*',
@@ -38,61 +19,30 @@ const client = new Client()
     'next-env.d.ts',
   ].join('\n'))
 
-  await new Promise<void>(resolve => {
-    client.exec(`rm -fr ${SSH_DIR}`, (err, steam) => {
-      resolve()
-    })
-  })
-
-  console.log('clean: done')
-
-  await mkdir([
-    SSH_DIR
-  ])
-  console.log('mkdir: done')
+  const zip = new JSZip()
 
   for await (const file of globber.globGenerator()) {
     const stats = await stat(file)
-    const path = `${SSH_DIR}/${relative('.', file)}`
+    const rPath = relative('.', file)
     if (stats.isDirectory()) {
-      await new Promise(resolve => {
-        sftp.stat(path, (err, stats) => {
-          if (!stats) sftp.mkdir(path, resolve)
-        })
-      })
+      zip.folder(rPath)
     } else {
-      await new Promise(resolve => {
-        sftp.fastPut(
-          file,
-          path,
-          resolve
-        )
-      })
-      console.log(`ok: ${path}`)
+      zip.file(rPath, await readFile(file))
     }
   }
 
-  console.log('upload: done')
+  const data = await zip.generateAsync({type: 'arraybuffer'})
 
-  client.exec(`cd ${SSH_DIR} && npm i && service website restart`, (err, steam) => {
-    steam.on('data', data => {
-      console.log(data.toString())
-      client.end()
-      client.destroy()
-    })
+  const formData = new FormData()
+  formData.append('file', Buffer.from(data), 'dist.zip')
+  formData.append('dir', DIR)
+  console.log('upload: start')
+  axios.post(URL, formData, {
+    maxContentLength: Infinity,
+    maxBodyLength: Infinity
+  }).then(() => {
+    console.log('upload: done')
+  }).catch(() => {
+    console.log('upload: failed')
   })
-
-  /**
-   *
-   * @param {Array<string>} dirs
-   */
-  async function mkdir(dirs) {
-    await Promise.all(dirs.map(dir => {
-      return new Promise(resolve => {
-        sftp.stat(dir, (err, stats) => {
-          if (!stats) sftp.mkdir(dir, resolve)
-        })
-      })
-    }))
-  }
 }()
